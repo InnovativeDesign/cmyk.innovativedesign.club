aad565ea9aab5394c1355df47502671e9fafe4fe
brian <mr.brianhobo@gmail.com>
2019-11-05 04:01:37 -0800

    add past cmyk photo slider

---
+++ assets/js/scripts.js
@@ -0,0 +1,102 @@
+(function(window) {
+
+	'use strict';
+
+	// the main slider and its items
+	var sliderEl = document.querySelector('.slider'),
+		items = [].slice.call(sliderEl.querySelectorAll('.slide')),
+		// total number of items
+		itemsTotal = items.length,
+		// navigation controls/arrows
+		navRightCtrl = sliderEl.querySelector('.button--nav-next'),
+		navLeftCtrl = sliderEl.querySelector('.button--nav-prev'),
+		// index of current item
+		current = 0;
+
+	function init() {
+		initEvents();
+	}
+
+	// event binding
+	function initEvents() {
+		// navigation
+		navRightCtrl.addEventListener('click', function() { navigate('right'); });
+		navLeftCtrl.addEventListener('click', function() { navigate('left'); });
+
+		// keyboard navigation events
+		document.addEventListener( 'keydown', function( ev ) {
+			var keyCode = ev.keyCode || ev.which;
+			switch (keyCode) {
+				case 37:
+					navigate('left');
+					break;
+				case 39:
+					navigate('right');
+					break;
+			}
+		} );
+	}
+
+	// navigate the slider
+	function navigate(dir) {
+		var itemCurrent = items[current],
+			currentEl = itemCurrent.querySelector('.slide__mover'),
+			currentTitleEl = itemCurrent.querySelector('.slide__title');
+
+		// update new current value
+		if( dir === 'right' ) {
+			current = current < itemsTotal-1 ? current + 1 : 0;
+		}
+		else {
+			current = current > 0 ? current - 1 : itemsTotal-1;
+		}
+
+		var itemNext = items[current],
+			nextEl = itemNext.querySelector('.slide__mover'),
+			nextTitleEl = itemNext.querySelector('.slide__title');
+		
+		// animate the current element out
+		dynamics.animate(currentEl, { opacity: 0, translateX: dir === 'right' ? -1*currentEl.offsetWidth/2 : currentEl.offsetWidth/2, rotateZ: dir === 'right' ? -10 : 10 }, {
+			type: dynamics.spring,
+			duration: 2000,
+			friction: 600,
+			complete: function() {
+				dynamics.css(itemCurrent, { opacity: 0, visibility: 'hidden' });
+			}
+		});
+
+		// animate the current title out
+		dynamics.animate(currentTitleEl, { translateX: dir === 'right' ? -250 : 250, opacity: 0 }, {
+			type: dynamics.bezier,
+			points: [{"x":0,"y":0,"cp":[{"x":0.2,"y":1}]},{"x":1,"y":1,"cp":[{"x":0.3,"y":1}]}],
+			duration: 450
+		});
+
+		// set the right properties for the next element to come in
+		dynamics.css(itemNext, { opacity: 1, visibility: 'visible' });
+		dynamics.css(nextEl, { opacity: 0, translateX: dir === 'right' ? nextEl.offsetWidth/2 : -1*nextEl.offsetWidth/2, rotateZ: dir === 'right' ? 10 : -10 });
+
+		// animate the next element in
+		dynamics.animate(nextEl, { opacity: 1, translateX: 0 }, {
+			type: dynamics.spring,
+			duration: 2000,
+			friction: 600,
+			complete: function() {
+				items.forEach(function(item) { classie.remove(item, 'slide--current'); });
+				classie.add(itemNext, 'slide--current');
+			}
+		});
+
+		// set the right properties for the next title to come in
+		dynamics.css(nextTitleEl, { translateX: dir === 'right' ? 250 : -250, opacity: 0 });
+		// animate the next title in
+		dynamics.animate(nextTitleEl, { translateX: 0, opacity: 1 }, {
+			type: dynamics.bezier,
+			points: [{"x":0,"y":0,"cp":[{"x":0.2,"y":1}]},{"x":1,"y":1,"cp":[{"x":0.3,"y":1}]}],
+			duration: 650
+		});
+	}
+
+	init();
+
+})(window);
